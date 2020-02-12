27890dca26dfb0114438c530bcdb857623267f05
Zachary Chandler <zachary.a.chandler96@gmail.com>
2020-02-11 18:40:40 -0600

    Include liscence in source file for FastNoise

--- src/main/java/org/ex/external/fast/FastNoise.java
+++ src/main/java/org/ex/external/fast/FastNoise.java
@@ -31,6 +31,29 @@ package org.ex.external.fast;
 import javax.vecmath.Vector2f;
 import javax.vecmath.Vector3f;
 
+/*
+MIT License
+
+Copyright (c) 2017 Jordan Peck
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
 public class FastNoise {
 	public enum NoiseType {Value, ValueFractal, Perlin, PerlinFractal, Simplex, SimplexFractal, Cellular, WhiteNoise, Cubic, CubicFractal}
 	public enum Interp {Linear, Hermite, Quintic}
