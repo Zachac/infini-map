2c89a85e103245b6633ba23daa4610324ca77496
Zachary Chandler <Zachary.A.Chandler96@gmail.com>
2020-03-19 15:36:24 -0500

    add shortcut keys for map movement actions

--- src/main/resources/public/main.js
+++ src/main/resources/public/main.js
@@ -1,4 +1,9 @@
 
+var x = 0
+var y = 0
+var radius = 10
+var zoom = 1
+
 function fetchMap(x, y, radius, zoom) {
 	fetch("graphql", {
 		method: "POST", 
@@ -14,4 +19,23 @@ function fetchMap(x, y, radius, zoom) {
 	)
 }
 
-fetchMap(0, 0, 10, 1)
+function updateMap() {
+	fetchMap(x, y, radius, zoom)	
+}
+
+updateMap()
+
+document.onkeydown = function(event) {
+	
+	switch (event.code) {
+	case "KeyW": x -= 1; break;
+	case "KeyA": y -= 1; break;
+	case "KeyS": x += 1; break;
+	case "KeyD": y += 1; break;
+	case "KeyZ": zoom *= 1.1; break;
+	case "KeyX": zoom /= 1.1; break;
+	case "KeyC": x = 0, y = 0, zoom = 1; break;
+	}
+	
+	updateMap()
+}
